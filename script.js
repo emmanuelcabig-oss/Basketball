1109ec0cf22e94e45033a05bf1d90568eeb2433e
emmanuelcabig-oss <emmanuelcabig@gmail.com>
2026-05-19 16:17:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+let scoreA = 0;
+let scoreB = 0;
+
+let gameSeconds = 0;
+let shotSeconds = 24;
+
+let gameInterval;
+let shotInterval;
+
+// SCORE
+function addPoints(team, points){
+    if(team === 'A'){
+        scoreA += points;
+        document.getElementById("scoreA").innerText = scoreA;
+    } else {
+        scoreB += points;
+        document.getElementById("scoreB").innerText = scoreB;
+    }
+}
+
+function removePoints(team, points){
+    if(team === 'A'){
+        scoreA -= points;
+        if(scoreA < 0) scoreA = 0;
+        document.getElementById("scoreA").innerText = scoreA;
+    } else {
+        scoreB -= points;
+        if(scoreB < 0) scoreB = 0;
+        document.getElementById("scoreB").innerText = scoreB;
+    }
+}
+
+// GAME TIMER
+function updateGame(){
+    let m = Math.floor(gameSeconds / 60);
+    let s = gameSeconds % 60;
+    s = s < 10 ? "0" + s : s;
+
+    document.getElementById("gameTimer").innerText = `${m}:${s}`;
+}
+
+function setGameTime(){
+    let mins = document.getElementById("minutesInput").value;
+    gameSeconds = mins * 60;
+    updateGame();
+}
+
+function startGameTimer(){
+    clearInterval(gameInterval);
+
+    gameInterval = setInterval(() => {
+        if(gameSeconds > 0){
+            gameSeconds--;
+            updateGame();
+        }
+    }, 1000);
+}
+
+function pauseGameTimer(){
+    clearInterval(gameInterval);
+}
+
+function resetGameTimer(){
+    clearInterval(gameInterval);
+    gameSeconds = 0;
+    updateGame();
+}
+
+// SHOT CLOCK
+function updateShot(){
+    document.getElementById("shotClock").innerText = shotSeconds;
+}
+
+function setShotClock(){
+    shotSeconds = document.getElementById("shotInput").value;
+    updateShot();
+}
+
+function startShotClock(){
+    clearInterval(shotInterval);
+
+    shotInterval = setInterval(() => {
+        if(shotSeconds > 0){
+            shotSeconds--;
+            updateShot();
+        }
+    }, 1000);
+}
+
+function pauseShotClock(){
+    clearInterval(shotInterval);
+}
+
+function resetShotClock(){
+    clearInterval(shotInterval);
+    shotSeconds = 24;
+    updateShot();
+}
+
+// RESET ALL
+function resetEverything(){
+    scoreA = 0;
+    scoreB = 0;
+
+    document.getElementById("scoreA").innerText = 0;
+    document.getElementById("scoreB").innerText = 0;
+
+    resetGameTimer();
+    resetShotClock();
+}
